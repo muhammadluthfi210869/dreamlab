82cf7656d039b90ce3f9b84d84dafce3b43145a9
Muhammad Luthfi <2966luthfi@gmail.com>
2026-06-16 09:53:23 +0700

    feat: add Google Site Verification, Microsoft Clarity, TikTok Pixel in head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,9 @@ export const metadata: Metadata = {
   metadataBase: new URL('https://dreamlab.id'),
   title: "Dreamlab | Maklon Kosmetik & Parfum BPOM Terbaik Indonesia",
   description: "One-Stop Maklon Kosmetik Bersertifikat BPOM, CPKB Grade A & Halal MUI di Surabaya. 500+ Brand Sudah Mempercayakan Formulasi & Produksinya pada Kami.",
+  verification: {
+    google: "xbEDjXMgxa8PqdCWCHtZ9-7xbPIgRcxc2azHdOLUBCE",
+  },
     icons: {
       icon: [{ url: "/assets/images/cropped-Logo-Dreamlab-Maklon-Kosmetik-.webp", sizes: "256x256", type: "image/webp" }],
       apple: "/assets/images/cropped-Logo-Dreamlab-Maklon-Kosmetik-.webp",
@@ -71,6 +74,29 @@ export default function RootLayout({
         <SpeculationRules />
         <OrganizationSchema />
         <TrackingScripts />
+
+        {/* Microsoft Clarity */}
+        <Script id="clarity" strategy="beforeInteractive">
+          {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "ttkbjtf1w5");`}
+        </Script>
+
+        {/* TikTok Pixel */}
+        <Script id="tiktok-pixel" strategy="beforeInteractive">
+          {`!function (w, d, t) {
+            w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
+            ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};
+            for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
+            ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e};
+            ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+            ttq.load('D0A8CHRC77UD5RFHJ6E0');
+            ttq.page();
+          }(window, document, 'ttq');`}
+        </Script>
+
         <div id="page" className="site">
           <Header />
           <div id="content" className="site-content">
@@ -83,19 +109,6 @@ export default function RootLayout({
           <Footer />
           <WhatsAppButton />
           <GTMNoScript />
-
-          {/* TikTok Pixel — beforeInteractive supaya terdeteksi Tag Explorer */}
-          <Script id="tiktok-pixel" strategy="beforeInteractive">
-            {`!function (w, d, t) {
-              w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
-              ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};
-              for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
-              ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e};
-              ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
-              ttq.load('D0A8CHRC77UD5RFHJ6E0');
-              ttq.page();
-            }(window, document, 'ttq');`}
-          </Script>
         </div>
       </body>
     </html>
