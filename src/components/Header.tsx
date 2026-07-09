85e7ac3b0d6a2e9239007ca5d055d421fc269be9
Revita <revitayustianawati7@gmail.com>
2026-07-09 09:32:31 +0700

    Auto update by OpenCode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -121,7 +121,7 @@ export default function Header() {
 
   return (
     <header className="absolute top-0 left-0 w-full z-50 bg-transparent transition-all duration-300">
-      <div className="container-custom flex items-center justify-between h-22 md:h-28">
+      <div className="max-w-[1400px] mx-auto px-6 lg:px-8 flex items-center justify-between h-22 md:h-28">
         {/* Left: Logo + All Menu Items */}
         <div className="flex items-center">
           <Link href="/" className="flex items-center shrink-0 group transition-transform duration-300">
